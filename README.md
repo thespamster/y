06d433ef248efad52b39d69e1b3ef89b0960fd5c
Gavin Brown <iamslowtrain@gmail.com>
2024-09-08 16:57:19 +0100

    update README. add link to 4uxdesign on not a link page.

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# y
+# Y
 
 ** UNDER CONSTRUCTION **
 
-A spoof conspiracy / news site to allow me to experiment with ai image generation and different layouts.
+A spoof conspiracy / news site to allow me to experiment with ai image generation and different layouts. This site will be uploaded to Wordpress when complete.
 
+The site has been built using HTML5, CSS3 and Javascript. The only library used is the Bootstrap Icon library that provides the SVG icons used in the footer. All the code is vanilla HTML5, CSS3 and Javascript just to sho that I can code completely from scratch.
 
+The site is responsive and scales for mobile, tablet and desktop sizes. All of the responsiveness is built in by myself and features a hamburger menu that appears on mobile screens.
 
 
 ## acknowledgements
 
 [favicon.io](https://favicon.io/) - favicon generator
+
+[Bing AI Image Creator](https://www.bing.com/images/create?FORM=GDPGLP) - AI images for free
+
+[Bootstrap Icons](https://icons.getbootstrap.com/) - Free SVG Icons from Bootstrap
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,29 @@
-console.log('JS loaded');
-
 document.getElementById('hamburgerToggle').onclick = function() {
     var element = document.getElementById("hamMenuToggle");
     element.classList.toggle("display-none");
-
 }
 
 function navbarShrink (x) {
     if (x.matches) {
         var navbarMenu = document.getElementById("navbarMenu");
         navbarMenu.classList.add("display-none");
-
         var headerIcon = document.getElementById("headerIcon");
         headerIcon.classList.remove("header-icon");
         headerIcon.classList.add("header-icon-small");
-
         var hamburgerIcon = document.getElementById("hamburgerToggle");
         hamburgerIcon.classList.remove("display-none");
-
-
     } else {
         var navbarMenu = document.getElementById("navbarMenu");
         navbarMenu.classList.remove("display-none");
-
         var headerIcon = document.getElementById("headerIcon");
         headerIcon.classList.remove("header-icon-small");
         headerIcon.classList.add("header-icon");
-
         var hamburgerIcon = document.getElementById("hamburgerToggle");
         hamburgerIcon.classList.add("display-none");
     }
 }
 
 var x = window.matchMedia('(max-width: 576px)');
-
 navbarShrink(x);
 
 x.addEventListener('change', function() {
